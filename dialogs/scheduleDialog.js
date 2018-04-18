300ef9a208434efa173a90deeb46314195f35255
OlesZadorozhnyy <oleszadorozhnyy@gmail.com>
2018-04-18 16:53:46 +0300

    Add new line

--- dialogs/scheduleDialog.js
+++ dialogs/scheduleDialog.js
@@ -10,12 +10,12 @@ module.exports = function(session) {
 function getSchedule() {
 	let text = '';
 
-	text += 'I пара (8:00 – 9:20)';
-	text += 'II пара (9:35 – 10:55)';
-	text += 'III пара (11:10 – 12:30)';
-	text += 'IV пара (12:45 – 14:05)';
-	text += 'V пара (14:20 – 15:40)';
-	text += 'VI пара (15:55 – 17:15)';
+	text += 'I пара (8:00 – 9:20)\n' ;
+	text += 'II пара (9:35 – 10:55)\n';
+	text += 'III пара (11:10 – 12:30)\n';
+	text += 'IV пара (12:45 – 14:05)\n';
+	text += 'V пара (14:20 – 15:40)\n';
+	text += 'VI пара (15:55 – 17:15)\n';
 	text += 'VII пара (17:30 – 19:00)';
 
 	return text;
