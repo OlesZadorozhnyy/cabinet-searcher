c8b1d609a281c5f2e55a96d1a82fb5820d895515
OlesZadorozhnyy <oleszadorozhnyy@gmail.com>
2018-04-18 14:43:47 +0300

    Change place for recognizer

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const builder = require('botbuilder');
 const connector = require('./connector');
 const rootDialog = require('./dialogs/rootDialog');
 
-const server = restify.createServer();
 const bot = new builder.UniversalBot(connector, rootDialog);
+require('./recognizer')(bot);
 
+const server = restify.createServer();
+
+// routes
 require('./routes/bot')(bot);
 require('./routes/web')(server, connector);
-require('./recognizer')(bot);
 
 server.listen(process.env.port || process.env.PORT || 3978, () => {
 	console.log('%s listening to %s', server.name, server.url); 
